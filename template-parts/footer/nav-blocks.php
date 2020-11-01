c95acd73843f0e2c242afe5936cc8177af3ddad0
raphaelreimann <reimannraphael@gmail.com>
2020-11-01 12:29:06 +0100

    footer nav colors

--- template-parts/footer/nav-blocks.php
+++ template-parts/footer/nav-blocks.php
@@ -1,13 +1,13 @@
 <div class="footer-nav-block container-fluid">
   <div class="row ">
-   <a href="/about" class="col-md-4 block block-1">
-     <span>Who We Are</span>
-   </a>
-   <a href="/green-new-deal" class="col-md-4 block block-2">
-    <span> What We Want</span>
-   </a>
-   <a href="/take-action" class="col-md-4 block block-3">
-    <span> How To Join</span>
-   </a>
-</div>
+    <a href="/ueber-uns" class="col-md-4 block block-1">
+      <span>Wer wir sind</span>
+    </a>
+    <a href="/forderungen" class="col-md-4 block block-2">
+      <span> Was wir fordern</span>
+    </a>
+    <a href="/mitmachen" class="col-md-4 block block-3">
+      <span> Wie Du mitmachen kannst</span>
+    </a>
+  </div>
 </div>
